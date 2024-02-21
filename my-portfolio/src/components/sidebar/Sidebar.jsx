e1c7939411d65dbde863d54ca180da045d72f9c3
Stefko1998 <Simjanovski998@gmail.com>
2024-02-21 17:15:32 +0100

    creating&styling sidebar as navbar

--- my-portfolio/src/components/sidebar/Sidebar.jsx
+++ my-portfolio/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,69 @@
-import { Link } from 'react-router-dom';
-// import LogoS from '../../assets/logo-s.png'
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import LogoS from '../../assets/logo-s.png'
 import './Sidebar.scss';
 
 const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            {/* <img src={LogoS} /> */}
-        </Link>
-    </div>
+  <div className='nav-bar'>
+    <Link className='logo' to='/'>
+      <img src={LogoS} />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className='about-link'
+        to="/about">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className='contact-link'
+        to="/contact">
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          target='_blank'
+          rel='noreferrer'
+          href='https://www.linkedin.com/in/stefan-simjanovski-a22b12236/'>
+          <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+        </a>
+      </li>
+      <li>
+        <a
+          target='_blank'
+          rel='noreferrer'
+          href='https://github.com/StefzZ8'>
+          <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+        </a>
+      </li>
+      <li>
+        <a
+          target='_blank'
+          rel='noreferrer'
+          href='https://www.instagram.com/'>
+          <FontAwesomeIcon icon={faInstagram} color='#4d4d4e' />
+        </a>
+      </li>
+      <li>
+        <a
+          target='_blank'
+          rel='noreferrer'
+          href='https://www.facebook.com/'>
+          <FontAwesomeIcon icon={faFacebook} color='#4d4d4e' />
+        </a>
+      </li>
+    </ul>
+  </div>
 )
 
 
