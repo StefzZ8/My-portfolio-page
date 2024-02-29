e1381feb86875588803735017610393faad3d89d
Stefko1998 <Simjanovski998@gmail.com>
2024-02-29 18:28:07 +0100

    changed colors & some structures

--- my-portfolio/src/components/sidebar/Sidebar.jsx
+++ my-portfolio/src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,8 @@
 import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import LogoS from '../../assets/logo-s.png'
+import { faHome, faUser, faEnvelope,  faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+import LogoS from '../../assets/logo-s.svg'
 import './Sidebar.scss';
 
 const Sidebar = () => (
@@ -20,6 +20,13 @@ const Sidebar = () => (
         className='about-link'
         to="/about">
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className='projects-link'
+        to="/projects">
+        <FontAwesomeIcon icon={faLaptopCode} color="#4d4d4e" />
       </NavLink>
       <NavLink
         exact="true"
