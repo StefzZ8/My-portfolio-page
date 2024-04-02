3a22325618ec4ab6c9bc709ae25fe12e8d642b43
Stefko1998 <Simjanovski998@gmail.com>
2024-04-02 12:25:02 +0200

    Button component implemented

--- my-portfolio/src/components/home-page/Homepage.jsx
+++ my-portfolio/src/components/home-page/Homepage.jsx
@@ -1,23 +1,29 @@
 import './Homepage.scss';
 import { Link } from 'react-scroll';
-import meImage from './../../assets/me.jpg';
+import Button from '../UI/Button';
 
 const Homepage = () => {
   return (
-    <section id='home'>
-      <div className='home__page-section'>
-        <span className='hello'>Hello,</span>
-        <span>I'm <span className='name'>Stefan.</span><br />Web Developer</span>
-        <h2>Junior Frontend Developer</h2>
-        <Link className='contact__me-btn' onClick={() => {
-          document.getElementById('contact').scrollIntoView({ behavior: 'smooth' })
-        }} >
+    <div id="home" className="home__page-section">
+      <span className="hello">Hello,</span>
+      <span>
+        I'm <span className="name">Stefan.</span>
+        <br />
+        Web Developer
+      </span>
+      <h2>Junior Frontend Developer</h2>
+      <Button>
+        <Link
+          onClick={() => {
+            document
+              .getElementById("contact")
+              .scrollIntoView({ behavior: "smooth" });
+          }}>
           CONTACT ME
         </Link>
-      </div>
-      {/* <img src={meImage} alt="" className='me-image' /> */}
-    </section>
-  )
-}
+      </Button>
+    </div>
+  );
+};
 
 export default Homepage;
