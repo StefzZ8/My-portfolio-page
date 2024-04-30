2713e586044b72e19cc393d6879eff65e09a59a9
Stefko1998 <Simjanovski998@gmail.com>
2024-04-30 20:58:03 +0200

    folder name changed

---
+++ my-portfolio/src/components/About/About.jsx
@@ -0,0 +1,21 @@
+import Skills from './Skills/Skills';
+import './About.scss';
+
+const About = () => {
+  return (
+    <div className='about-page' id='about'>
+      <div className='text-zone'>
+        <h1>About me</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa, possimus ut id magni cumque. Sunt qui vitae id harum architecto voluptatum. Blanditiis accusamus minus alias repellendus officia ad repellat!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa, possimus ut id magni cumque. Sunt qui vitae id harum architecto voluptatum. Blanditiis accusamus minus alias repellendus officia ad repellat!
+        </p>
+      </div>
+      <Skills />
+    </div>
+  )
+}
+
+export default About;
