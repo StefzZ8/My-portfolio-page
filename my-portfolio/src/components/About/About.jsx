ea1879f451e475c4386224eec240125793d6b26f
Stefko1998 <Simjanovski998@gmail.com>
2024-12-02 16:26:08 +0100

    about me text

--- my-portfolio/src/components/About/About.jsx
+++ my-portfolio/src/components/About/About.jsx
@@ -7,10 +7,7 @@ const About = () => {
       <div className='text-zone'>
         <h1>About me</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa, possimus ut id magni cumque. Sunt qui vitae id harum architecto voluptatum. Blanditiis accusamus minus alias repellendus officia ad repellat!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa, possimus ut id magni cumque. Sunt qui vitae id harum architecto voluptatum. Blanditiis accusamus minus alias repellendus officia ad repellat!
+          Hey, I'm Stefan, a front-end developer with a passion for growth! I'm all about learning, teamwork, and CrossFit. Eager to grow, I thrive in collaborative environments, bringing positivity and dedication to every project. Beyond work, you'll find me embracing the intensity of CrossFit, reflecting my commitment to discipline and community. Let's connect and achieve great things together!
         </p>
       </div>
       <Skills />
